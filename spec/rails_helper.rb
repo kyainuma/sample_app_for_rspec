2b1432d9742512aa73210f309f210875be9647a2
Yasunari_Kainuna <yasu6491@icloud.com>
2020-12-10 18:15:17 +0900

    Add モジュール読み込み

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+  Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -64,4 +64,7 @@ RSpec.configure do |config|
   
   # FactoryBotの利用をON
   config.include FactoryBot::Syntax::Methods
+
+  # module
+  config.include LoginModule
 end
