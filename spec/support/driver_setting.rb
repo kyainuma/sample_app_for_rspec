cc58d93153c7ba8c1debe7cc52ff9bfcd44188b2
Yasunari_Kainuna <yasu6491@icloud.com>
2020-12-13 23:14:30 +0900

    Add タスクのシステムスペック

--- spec/support/driver_setting.rb
+++ spec/support/driver_setting.rb
@@ -1,6 +1,7 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    # driven_by(:selenium_chrome) テスト実行時にブラウザが起動されます。byebugやsleepで停止して画面の要素を確認できます。
+    # テスト実行時にブラウザが起動されます。byebugやsleepで停止して画面の要素を確認できます。
+    # driven_by(:selenium_chrome)
     driven_by(:selenium_chrome_headless)
   end
 end
