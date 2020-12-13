6bc7f714fd2ba3f25e37d260e15fc77ae3159c47
Yasunari_Kainuna <yasu6491@icloud.com>
2020-12-13 16:06:38 +0900

    Add フォーム未入力の場合 ログイン処理が失敗する

--- spec/system/usersessions_spec.rb
+++ spec/system/usersessions_spec.rb
@@ -13,6 +13,17 @@ RSpec.describe "Usersessions", type: :system do
         expect(page).to have_content 'Login successful'
       end
     end
+    
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        fill_in 'Email', with: ''
+        fill_in 'Password', with: ''
+        click_button 'Login'
+        expect(current_path).to eq login_path
+        expect(page).to have_content 'Login failed'
+      end
+    end
   end
 
   describe 'ログイン後' do
